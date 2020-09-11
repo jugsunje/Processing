636c4650543a9a5715153f8033aea44226034019
jugsunje <jugsunje@gmail.com>
2020-09-11 14:54:57 +0900

    Create sketch_200911b.js

---
+++ sketch_200911b/sketch_200911b.js
@@ -0,0 +1,27 @@
+function setup() {
+createCanvas(500, 500);
+  
+  fill(200, 0, 0);  //
+  ellipse(50, 100, 100, 100);
+  fill(250, 200, 0);  //
+  ellipse(200, 100, 100, 100);
+  fill(0, 250, 0);  //
+  ellipse(100, 200, 100, 100);
+  fill(0, 0, 250);  //
+  ellipse(200, 200, 100, 100);
+  fill(100, 0, 0);  //
+  ellipse(150, 200, 100, 100);
+
+  
+}
+
+
+function draw() {
+
+
+}
+
+
+function draw() {
+
+}
