157f214578a79fdbd929b6ffd83397efc95f388f
jugsunje <jugsunje@gmail.com>
2020-09-25 14:38:03 +0900

    Update sketch_200925a.js

--- sketch_200911b/sketch_200918a/sketch_200925a/sketch_200925a.js
+++ sketch_200911b/sketch_200918a/sketch_200925a/sketch_200925a.js
@@ -1,12 +1,20 @@
+let size = 2;
+
 function setup() {
   createCanvas(500, 500);
-  
-  for(let i=0; i<500; i+=100){
-    rect(i, 0, 100, 100);
-  }
+  noStroke();
 }
 
-;
-function draw() {
 
+function draw() {
+  for(let i=0; i<500; i+=size){
+    for(let j=0; j<500; j+=size){
+      let red = int(random(0, 256));
+      let green = int(random(0, 256));
+      let blue = int(random(0, 256));
+
+      fill(red, green, blue);
+      rect(i, j, size, size);
+    }           
+  }
 }
